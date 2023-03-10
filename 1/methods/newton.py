e4282194917001a0709164ddc370d0c815ecd5e8
BogusKladik <boguskladik@gmail.com>
2023-03-11 00:15:04 +0300

    correction of the end-of-cycle condition

--- 1/methods/newton.py
+++ 1/methods/newton.py
@@ -1,11 +1,12 @@
 import numpy as np
+from utils import norm
 
 
 def newton(x0, gradient, hessian, accuracy):
     min_x1_x2, x1_x2 = x0 - \
         np.dot(gradient(x0[0], x0[1]), np.linalg.inv(hessian())), x0
 
-    while abs(min_x1_x2 - x1_x2).sum() >= len(min_x1_x2) * accuracy:
+    while norm(min_x1_x2 - x1_x2) >= accuracy:
         x1_x2, min_x1_x2 = min_x1_x2, x1_x2 - \
             np.dot(gradient(x1_x2[0], x1_x2[1]), np.linalg.inv(hessian()))
 
--- 1/utils.py
+++ 1/utils.py
@@ -8,3 +8,7 @@ def count_data(accuracy):
     n = 10**decimal_places(accuracy)
 
     return n
+
+
+def norm(vector):
+    return sum(list(map(lambda x: abs(x)**len(vector), vector)))**(1 / len(vector))

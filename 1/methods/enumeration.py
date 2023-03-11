be2aa0fc6182f4bc2b47a6ed8854c1165c41f91f
BogusKladik <boguskladik@gmail.com>
2023-03-11 16:20:34 +0300

    correct method enumeration

--- 1/methods/enumeration.py
+++ 1/methods/enumeration.py
@@ -8,8 +8,9 @@ def enumeration(interval_x1, interval_x2, f, accuracy):
     X2 = np.linspace(interval_x2[0], interval_x2[1], n)
 
     min_x1_x2 = (X1[0], X2[0])
-    for i in range(n):
-        if f(X1[i], X2[i]) < f(min_x1_x2[0], min_x1_x2[1]):
-            min_x1_x2 = (X1[i], X2[i])
+    for x1 in X1:
+        for x2 in X2:
+            if f(x1, x2) < f(min_x1_x2[0], min_x1_x2[1]):
+                min_x1_x2 = (x1, x2)
 
     return min_x1_x2
